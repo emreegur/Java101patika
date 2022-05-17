dde175d28654ed23da3056a8a877b3ba09acdc97
emreegur <99649243+emreegur@users.noreply.github.com>
2022-05-17 15:35:03 +0300

    Add files via upload

---
+++ Bolum5Odev2MukemmelSayiBulma.java
@@ -0,0 +1,21 @@
+package Giris;
+import java.util.Scanner;
+public class Bolum5Odev2MukemmelSayiBulma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean dongu= true;
+        int sorgu= 0;
+        while(dongu){
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = input.nextInt();
+        for(int i=1;i <sayi;i++){
+            if(sayi % i == 0){
+                sorgu += i;
+            } } if (sorgu == sayi){
+                System.out.println(sayi + " Mükemmel sayıdır.");
+            } else{
+                System.out.println(sayi + " Mükemmel sayı değildir.");
+            }
+        }
+        }
+}
